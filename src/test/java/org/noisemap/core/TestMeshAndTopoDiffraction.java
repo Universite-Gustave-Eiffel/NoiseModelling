255d375130faaa0476b6f24ed98e9cd8e7484f1e
SU Qi <008@192.168.182.172>
2013-09-06 09:41:29 +0200

    Complet the test case

--- src/test/java/org/noisemap/core/TestMeshAndTopoDiffraction.java
+++ src/test/java/org/noisemap/core/TestMeshAndTopoDiffraction.java
@@ -69,28 +69,25 @@ public class TestMeshAndTopoDiffraction extends TestCase{
          public void testTopoPointsBlockSourceAndReceiver() throws LayerDelaunayError{
             
              GeometryFactory factory = new GeometryFactory();
-             Coordinate[] building1Coords = { new Coordinate(15., 5.,2.),
-                                        new Coordinate(15., 30.,3.), new Coordinate(30., 30.,5.),
-                                        new Coordinate(30., 5.,2.), new Coordinate(15., 5.,2.) };
-             Coordinate[] building2Coords = { new Coordinate(40., 5.,2.),
+
+             Coordinate[] building1Coords = { new Coordinate(40., 5.,2.),
                                         new Coordinate(45., 5.,3.), new Coordinate(45., 45.,5.),
                                         new Coordinate(40., 45.,2.), new Coordinate(40., 5.,2.) };
             
-             Coordinate topoPoint1 = new Coordinate(5.,10.,4.);
+             Coordinate topoPoint1 = new Coordinate(30.,15.,6.);
+
              Polygon building1 = factory.createPolygon(
-                            factory.createLinearRing(building1Coords), null);
-             Polygon building2 = factory.createPolygon(
-                            factory.createLinearRing(building2Coords), null);            
+                            factory.createLinearRing(building1Coords), null);            
              MeshBuilder mesh= new MeshBuilder();
-             mesh.addGeometry(building1,4.0);
-             mesh.addGeometry(building2,5.0);
+
+             mesh.addGeometry(building1,1.0);
              mesh.addTopograhic(topoPoint1);
 
              mesh.finishPolygonFeeding(new Envelope(new Coordinate(0., 0.,0.), new Coordinate(60., 60.,0.)));
              NewFastObstructionTest nfot= new NewFastObstructionTest(mesh.getPolygonWithHeight(),mesh.getTriangles(),mesh.getTriNeighbors(),mesh.getVertices());
-             System.out.println("----------TEST#2 diffraction with 2 buildings(building1 and building2)----- ");
+             System.out.println("----------TEST#2 diffraction blocked by TopoPoint----- ");
             
-             Double[] lt=nfot.getPath(new Coordinate(48,25,7), new Coordinate(5,15,8));
+             Double[] lt=nfot.getPath(new Coordinate(48,25,4), new Coordinate(5,15,3.7));
              System.out.println("----deltadistance----");
              System.out.println(lt[nfot.Delta_Distance]);
              System.out.println("----e----");
@@ -102,7 +99,7 @@ public class TestMeshAndTopoDiffraction extends TestCase{
             
             
              System.out.println("-----------exchange source receiver------------");
-             Double[]lt1=nfot.getPath(new Coordinate(5,15,8), new Coordinate(48,25,7));
+             Double[]lt1=nfot.getPath(new Coordinate(5,15,3.7), new Coordinate(48,25,4));
              System.out.println("----deltadistance----");
              System.out.println(lt1[nfot.Delta_Distance]);
              System.out.println("----e----");
@@ -111,11 +108,10 @@ public class TestMeshAndTopoDiffraction extends TestCase{
              System.out.println(lt1[nfot.Full_Difrraction_Distance]);
             
             
-             assertTrue("Exchange source receiver got the different resultat",lt[nfot.Delta_Distance]-lt1[nfot.Delta_Distance]<=FastObstructionTest.epsilon
-                       &&lt[nfot.E_Length]-lt1[nfot.E_Length]<=FastObstructionTest.epsilon&&lt[nfot.Full_Difrraction_Distance]-lt1[nfot.Full_Difrraction_Distance]<=FastObstructionTest.epsilon);
-             System.out.println("----------TEST#2 diffraction with 2 buildings(building1 and building2) finished----- ");
-             System.out.println("-----the intersections points------");
-
+            assertTrue("testTopoPointsBlockSourceAndReceiver failed! ",lt[nfot.Delta_Distance].equals(lt1[nfot.Delta_Distance])
+                       &&lt[nfot.E_Length].equals(lt1[nfot.E_Length])&&lt[nfot.Full_Difrraction_Distance].equals(lt1[nfot.Full_Difrraction_Distance])&&lt[nfot.Delta_Distance].equals(-1.0));
+             System.out.println("----------TEST#2 diffraction blocked by TopoPoint finished----- ");
+            
 
          }
          
@@ -191,7 +187,7 @@ public class TestMeshAndTopoDiffraction extends TestCase{
              assertTrue("testIfSourceOrReceiverAreImporerWithTopographic failed! ",lt[nfot.Delta_Distance].equals(lt1[nfot.Delta_Distance])
                        &&lt[nfot.E_Length].equals(lt1[nfot.E_Length])&&lt[nfot.Full_Difrraction_Distance].equals(lt1[nfot.Full_Difrraction_Distance])&&lt[nfot.Delta_Distance].equals(-1.0));
             
-
+             System.out.println("----------TEST#3 diffraction with 2 buildings(building1 and building2) finished----- ");
 
          }
 }
