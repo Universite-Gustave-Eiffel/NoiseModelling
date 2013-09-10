a7898fa9ee2e2f7dd05e047fc0d0885a650bd828
SU Qi <008@192.168.182.172>
2013-09-10 17:00:34 +0200

    modify the test case of SoilEffet

--- src/test/java/org/noisemap/core/TestSoilEffet.java
+++ src/test/java/org/noisemap/core/TestSoilEffet.java
@@ -17,7 +17,7 @@ import junit.framework.TestCase;
  */
 public class TestSoilEffet extends TestCase{
     
-    public void testSoilEffet() throws LayerDelaunayError{
+    public void testSoilEffet1() throws LayerDelaunayError{
         GeometryFactory factory = new GeometryFactory();
         Coordinate[] building1Coords = { new Coordinate(15., 5.,2.),
                                         new Coordinate(15., 30.,3.), new Coordinate(30., 30.,5.),
@@ -28,7 +28,7 @@ public class TestSoilEffet extends TestCase{
         Coordinate topoPoint1 = new Coordinate(5.,10.,1.);
         Polygon building1 = factory.createPolygon(
                             factory.createLinearRing(building1Coords), null);
-        Coordinate[] geoSoil1={new Coordinate(7.,5.,0.), new Coordinate(7.,30.,0.),
+        Coordinate[] geoSoil1 = {new Coordinate(7.,5.,0.), new Coordinate(7.,30.,0.),
                                new Coordinate(10.,13.,0.), new Coordinate(14.,30.,0.), 
                                new Coordinate(14.,5.,0.), new Coordinate(7.,5.,0.)}; 
         MeshBuilder mesh= new MeshBuilder();
@@ -39,18 +39,57 @@ public class TestSoilEffet extends TestCase{
         FastObstructionTest nfot= new FastObstructionTest(mesh.getPolygonWithHeight(),mesh.getTriangles(),mesh.getTriNeighbors(),mesh.getVertices());
         
         nfot.addGeoSoil(factory.createPolygon(geoSoil1), 0.7);
-        nfot.isCalSoilEffet=true;
-        nfot.checkGeoSoil=true;
-        System.out.println("----------TEST#1 diffraction with 1 buildings----- ");
+
+        
+        System.out.println("----------TEST#1 diffraction with 1 building last path has soil effet----- ");
 
         Double[]lt=nfot.getPath(new Coordinate(48,25,7), new Coordinate(5,15,8));
-        System.out.println("----deltadistance----");
-        System.out.println(lt[nfot.Delta_Distance]);
-        System.out.println("----e----");
-        System.out.println(lt[nfot.E_Length]);
-        System.out.println("----distancepath----");
-        System.out.println(lt[nfot.Full_Difrraction_Distance]);
-        System.out.println("----full distance with soil effet----");
-        System.out.println(lt[nfot.Full_Distance_With_Soil_Effet]);    
+
+
+        
+        System.out.println("----------TEST#1 diffraction with 1 building last path has soil effet finished----- ");
     }
+
+    public void testSoilEffet2() throws LayerDelaunayError{
+        GeometryFactory factory = new GeometryFactory();
+        Coordinate[] building1Coords = { new Coordinate(15., 5.,2.),
+                                        new Coordinate(15., 30.,3.), new Coordinate(30., 30.,5.),
+                                        new Coordinate(30., 5.,2.), new Coordinate(15., 5.,2.) };
+        
+        
+        
+        Coordinate topoPoint1 = new Coordinate(5.,10.,1.);
+        Polygon building1 = factory.createPolygon(
+                            factory.createLinearRing(building1Coords), null);
+        Coordinate[] geoSoil1 = {new Coordinate(7.,5.,0.), new Coordinate(7.,30.,0.),
+                               new Coordinate(10.,13.,0.), new Coordinate(14.,30.,0.), 
+                               new Coordinate(14.,5.,0.), new Coordinate(7.,5.,0.)};
+        Coordinate[] geoSoil2 = {new Coordinate(35.,10.,0.), new Coordinate(35.,40.,0.),
+                               new Coordinate(40.,40.,0.), new Coordinate(40.,10.,0.), 
+                               new Coordinate(35.,10.,0.)};
+        Coordinate[] geoSoil3 = {new Coordinate(45.,10.,0.), new Coordinate(45.,40.,0.),
+                               new Coordinate(55.,40.,0.), new Coordinate(55.,10.,0.), 
+                               new Coordinate(45.,10.,0.)};
+        
+        MeshBuilder mesh= new MeshBuilder();
+        mesh.addGeometry(building1,6.0);
+        mesh.addTopograhicPoint(topoPoint1);
+        mesh.finishPolygonFeeding(new Envelope(new Coordinate(0., 0.,0.), new Coordinate(60., 60.,0.)));
+
+        FastObstructionTest nfot= new FastObstructionTest(mesh.getPolygonWithHeight(),mesh.getTriangles(),mesh.getTriNeighbors(),mesh.getVertices());
+        
+        nfot.addGeoSoil(factory.createPolygon(geoSoil1), 0.7);
+        //calculate soil effet
+
+        //debug mode
+
+        
+        System.out.println("----------TEST#2 diffraction with 1 building first path and last path have soil effet----- ");
+
+        Double[]lt=nfot.getPath(new Coordinate(48,25,7), new Coordinate(5,15,8));
+
+
+        
+        
+    }    
 }
