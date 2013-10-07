0dee7b1b8d58dd637f8157a487854e00530a5403
nicolas-f <github@nettrader.fr>
2013-10-07 13:23:26 +0200

    Replace point grid by segment grid

--- src/main/java/org/noisemap/core/LayerJDelaunay.java
+++ src/main/java/org/noisemap/core/LayerJDelaunay.java
@@ -41,8 +41,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Stack;
 
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.LineSegment;
+import com.vividsolutions.jts.index.strtree.STRtree;
 import org.apache.log4j.Logger;
 
 import org.jdelaunay.delaunay.ConstrainedMesh;
@@ -78,6 +82,7 @@ public class LayerJDelaunay implements LayerDelaunay {
 	private boolean debugMode=false; //output primitives in a text file
 	private boolean computeNeighbors=false;
 	List<Triangle> triangles = new ArrayList<Triangle>();
+    private final static int GRID_PROP = 4;
 	private List<Triangle> neighbors = new ArrayList<Triangle>(); // The
 																		// first
 																		// neighbor
@@ -181,12 +186,31 @@ public class LayerJDelaunay implements LayerDelaunay {
 
 	private ConstrainedMesh delaunayTool = null;
 
+    /**
+     * This function return insertSegment if insertSegment is not collinear with staticSegment.
+     * Otherwise it return the difference of insertSegment and staticSegment
+     * @param factory Geometry factory
+     * @param staticSegment Segment one
+     * @param insertSegment Segment two
+     * @return LineSegment or null
+     */
+    private static LineSegment[] computeIntersection(GeometryFactory factory, LineSegment staticSegment, LineSegment insertSegment) {
+        LineString staticSegmentLine = staticSegment.toGeometry(factory);
+        LineString insertSegmentLine = insertSegment.toGeometry(factory);
+        Geometry diff = staticSegmentLine.difference(insertSegmentLine);
+        if(diff instanceof LineString) {
+            return new LineSegment[] { insertSegment };
+        } else {
+            return new LineSegment[] { insertSegment };
+        }
+    }
+
 	@Override
 	public void processDelaunay() throws LayerDelaunayError {
 		if (delaunayTool != null && (!ptToInsert.isEmpty() || !constraintEdge.isEmpty())) {
 			try {
                 if(maxArea != null) {
-                    List<DPoint> dpoints = new LinkedList<DPoint>(ptToInsert);
+                    ArrayList<DEdge> edges = new ArrayList<DEdge>(constraintEdge);
                     // Build a PointsMerge tool to not add duplicates
                     Envelope gridEnv = null;
                     if(!ptToInsert.isEmpty()) {
@@ -194,14 +218,10 @@ public class LayerJDelaunay implements LayerDelaunay {
                     } else if(!constraintEdge.isEmpty()) {
                         gridEnv = new Envelope(constraintEdge.get(0).getStartPoint().getCoordinate());
                     }
-                    PointsMerge merge = new PointsMerge(EPSILON_AREA_CONSTRAINT);
                     for(DPoint dPoint : ptToInsert) {
-                        merge.getOrAppendVertex(dPoint.getCoordinate());
                         gridEnv.expandToInclude(dPoint.getCoordinate());
                     }
                     for(DEdge edge : constraintEdge) {
-                        merge.getOrAppendVertex(edge.getStartPoint().getCoordinate());
-                        merge.getOrAppendVertex(edge.getEndPoint().getCoordinate());
                         gridEnv.expandToInclude(edge.getStartPoint().getCoordinate());
                         gridEnv.expandToInclude(edge.getEndPoint().getCoordinate());
                     }
@@ -214,21 +234,46 @@ public class LayerJDelaunay implements LayerDelaunay {
                     double yCount = Math.ceil((gridEnv.getMaxY() - gridEnv.getMinY()) / requestedDeltaGrid);
                     double xDelta = (gridEnv.getMaxX() - gridEnv.getMinX()) / xCount;
                     double yDelta = (gridEnv.getMaxY() - gridEnv.getMinY()) / yCount;
+                    GeometryFactory factory = new GeometryFactory();
+                    // Set negative weigths for grid  as this is less important constraint as polygons.
+                    Map<Integer, Integer> weights = new HashMap<Integer, Integer>();
+                    weights.put(GRID_PROP, -1);
+                    delaunayTool.setWeights(weights);
                     for(int xi = 0; xi < xCount; xi++) {
                         for(int yi = 0; yi < yCount; yi++) {
                             Coordinate gridPoint = new Coordinate(gridEnv.getMinX() + xi * xDelta, gridEnv.getMinY() + yi * yDelta, 0);
-                            // If this point does not belong to an existing mesh constraint
-                            if(merge.getOrAppendVertex(gridPoint) + 1 == merge.getSize()) {
-                                dpoints.add(new DPoint(gridPoint));
+                            LineSegment horizontal = new LineSegment(gridPoint, new Coordinate(gridEnv.getMinX() + (xi + 1) * xDelta, gridEnv.getMinY() + yi * yDelta, 0));
+                            LineSegment vertical = new LineSegment(gridPoint, new Coordinate(gridEnv.getMinX() + xi * xDelta, gridEnv.getMinY() + (yi + 1) * yDelta, 0));
+                            // Insert horizontal constraint
+                            /**
+                            List results = colinearTestRTree.query(new Envelope(horizontal.p0, horizontal.p1));
+                            boolean doInsert = true;
+                            LineString segment = factory.createLineString()
+                            for(Object index : results) {
+                                if(index instanceof Integer) {
+                                    DEdge edge = constraintEdge.get((Integer) index);
+                                    // If grid segment is partially colinear with edge then update grid segment to complete the line
+                                    // if grid segment is totally colinear, do not include the edge
+
+                                }
                             }
+                            */
+                            DEdge hEdge = new DEdge(new DPoint(horizontal.p0), new DPoint(horizontal.p1));
+                            hEdge.setProperty(GRID_PROP);
+                            edges.add(hEdge);
+                            DEdge vEdge = new DEdge(new DPoint(vertical.p0), new DPoint(vertical.p1));
+                            vEdge.setProperty(GRID_PROP);
+                            edges.add(vEdge);
                         }
                     }
-                    delaunayTool.setPoints(dpoints);
+                    delaunayTool.setPoints(ptToInsert);
+                    // Push segments
+                    delaunayTool.setConstraintEdges(edges);
                 } else {
                     delaunayTool.setPoints(ptToInsert);
+                    // Push segments
+                    delaunayTool.setConstraintEdges(constraintEdge);
                 }
-                // Push segments
-                delaunayTool.setConstraintEdges(constraintEdge);
 				
 				if(debugMode) {
 					try
@@ -296,7 +341,7 @@ public class LayerJDelaunay implements LayerDelaunay {
 							navHistoryDir.pop();							
 						} else {
 							DEdge ed = navHistoryTri.peek().getEdge(navHistoryDir.peek());
-							if(!ed.isLocked()) {
+							if(!ed.isLocked() || ed.getProperty() == GRID_PROP) {
 								DTriangle neigh=ed.getOtherTriangle(navHistoryTri.peek());
 								if(neigh != null) {
 									if(neigh.getExternalGID()!=0) { //Not set as destroyed
