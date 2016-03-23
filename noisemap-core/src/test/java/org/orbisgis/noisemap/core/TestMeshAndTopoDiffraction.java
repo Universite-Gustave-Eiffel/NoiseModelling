9cd97cd8b619c092e3a1f78117d4e8526685588c
nicolas-f <github@nettrader.fr>
2016-03-23 10:55:03 +0100

    remove old gdms project and update license

--- noisemap-core/src/test/java/org/orbisgis/noisemap/core/TestMeshAndTopoDiffraction.java
+++ noisemap-core/src/test/java/org/orbisgis/noisemap/core/TestMeshAndTopoDiffraction.java
@@ -1,20 +1,20 @@
 
-/**
+/*
  * NoiseMap is a scientific computation plugin for OrbisGIS developed in order to
- * evaluate the noise impact on urban mobility plans. This model is
+ * evaluate the noise impact in urban areas. This model is
  * based on the French standard method NMPB2008. It includes traffic-to-noise
  * sources evaluation and sound propagation processing.
  *
- * This version is developed at French IRSTV Institute and at IFSTTAR
- * (http://www.ifsttar.fr/) as part of the Eval-PDU project, funded by the
+ * This plugin is currently developed by the Environmental Acoustics Laboratory (LAE) of Ifsttar
+ * (http://wwww.lae.ifsttar.fr/) in collaboration with the Lab-STICC CNRS laboratory.
+ * It was initially developed as part of the Eval-PDU project, funded by the
  * French Agence Nationale de la Recherche (ANR) under contract ANR-08-VILL-0005-01.
  *
  * Noisemap is distributed under GPL 3 license. Its reference contact is Judicaël
  * Picaut <judicael.picaut@ifsttar.fr>. It is maintained by Nicolas Fortin
- * as part of the "Atelier SIG" team of the IRSTV Institute <http://www.irstv.fr/>.
+ * <nicolas.fortin@ifsttar.fr>
  *
- * Copyright (C) 2011 IFSTTAR
- * Copyright (C) 2011-2012 IRSTV (FR CNRS 2488)
+ * Copyright (C) 2011-2016 IFSTTAR-CNRS
  *
  * Noisemap is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
@@ -28,8 +28,11 @@
  * You should have received a copy of the GNU General Public License along with
  * Noisemap. If not, see <http://www.gnu.org/licenses/>.
  *
- * For more information, please consult: <http://www.orbisgis.org/>
+ * For more information concerning NoiseM@p, please consult: <http://noisemap.orbisgis.org/>
+ *
+ * For more information concerning OrbisGis, please consult: <http://www.orbisgis.org/>
  * or contact directly:
+ *
  * info_at_ orbisgis.org
  */
 package org.orbisgis.noisemap.core;
