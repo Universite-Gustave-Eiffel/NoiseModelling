9af20805fa1e3f1d46195fc0d355e492b7325183
nicolas-f <[omitted]>
2018-04-25 13:30:02 +0200

    Fix issue when computing an area with only 1 source or 1 building

--- noisemap-h2/src/test/java/org/orbisgis/noisemap/h2/TriGridTest.java
+++ noisemap-h2/src/test/java/org/orbisgis/noisemap/h2/TriGridTest.java
@@ -143,6 +143,53 @@ public class TriGridTest {
         }
     }
 
+
+    @Test
+    public void testNoSource() throws SQLException {
+        // Create empty buildings table
+        st.execute("DROP TABLE IF EXISTS BUILDINGS");
+        st.execute("CREATE TABLE BUILDINGS(the_geom geometry)");
+        st.execute("INSERT INTO buildings (the_geom) VALUES (ST_GeomFromText('MULTIPOLYGON (((5 20 0,20 20 0,20 60 0,0 60 0,5 20 0)))'))");
+        // Create a single sound source
+        st.execute("DROP TABLE IF EXISTS roads_src_global");
+        st.execute("CREATE TABLE roads_src_global(the_geom POINT, db_m double)");
+        // Compute spectrum repartition
+        st.execute("drop table if exists roads_src;\n" +
+                "CREATE TABLE roads_src AS SELECT the_geom,\n" +
+                "BR_SpectrumRepartition(100,1,db_m) as db_m100,\n" +
+                "BR_SpectrumRepartition(125,1,db_m) as db_m125,\n" +
+                "BR_SpectrumRepartition(160,1,db_m) as db_m160,\n" +
+                "BR_SpectrumRepartition(200,1,db_m) as db_m200,\n" +
+                "BR_SpectrumRepartition(250,1,db_m) as db_m250,\n" +
+                "BR_SpectrumRepartition(315,1,db_m) as db_m315,\n" +
+                "BR_SpectrumRepartition(400,1,db_m) as db_m400,\n" +
+                "BR_SpectrumRepartition(500,1,db_m) as db_m500,\n" +
+                "BR_SpectrumRepartition(630,1,db_m) as db_m630,\n" +
+                "BR_SpectrumRepartition(800,1,db_m) as db_m800,\n" +
+                "BR_SpectrumRepartition(1000,1,db_m) as db_m1000,\n" +
+                "BR_SpectrumRepartition(1250,1,db_m) as db_m1250,\n" +
+                "BR_SpectrumRepartition(1600,1,db_m) as db_m1600,\n" +
+                "BR_SpectrumRepartition(2000,1,db_m) as db_m2000,\n" +
+                "BR_SpectrumRepartition(2500,1,db_m) as db_m2500,\n" +
+                "BR_SpectrumRepartition(3150,1,db_m) as db_m3150,\n" +
+                "BR_SpectrumRepartition(4000,1,db_m) as db_m4000,\n" +
+                "BR_SpectrumRepartition(5000,1,db_m) as db_m5000 from roads_src_global;");
+        // Compute noise map
+        ResultSet rs = st.executeQuery("SELECT * FROM BR_TRIGRID((SELECT ST_ENVELOPE(the_geom) from buildings),'buildings', 'roads_src', 'DB_M','', 700, 100,1,3,50,2,1,0.2)");
+        try {
+            assertTrue(rs.next());
+            do {
+                assertTrue(rs.getDouble("W_V1") >= 1);
+                assertTrue(rs.getDouble("W_V2") >= 1);
+                assertTrue(rs.getDouble("W_V3") >= 1);
+                assertNotNull(rs.getObject("THE_GEOM"));
+                assertTrue(rs.getObject("THE_GEOM") instanceof Polygon);
+                assertEquals(4, ((Polygon) rs.getObject("THE_GEOM")).getNumPoints());
+            } while (rs.next());
+        } finally {
+            rs.close();
+        }
+    }
     @Test
     public void testMultipleBuildings() throws Exception {
         st.execute(getRunScriptRes("multiple_buildings.sql"));
