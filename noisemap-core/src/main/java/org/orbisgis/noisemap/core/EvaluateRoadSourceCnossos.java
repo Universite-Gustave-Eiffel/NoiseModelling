d2475907adb41959596fde3424cc748d21909e5d
pierromond <pierromond@hotmail.com>
2018-08-17 10:45:44 +0200

    Update French road pavement coeff

--- noisemap-core/src/main/java/org/orbisgis/noisemap/core/EvaluateRoadSourceCnossos.java
+++ noisemap-core/src/main/java/org/orbisgis/noisemap/core/EvaluateRoadSourceCnossos.java
@@ -207,6 +207,73 @@ public class EvaluateRoadSourceCnossos {
             {-4.9,-0.7,-0.7,0.,0.,0.},
             {-3.3,-0.2,-0.2,0.,0.,0.},
             {-1.5,0.5,0.5,0.,0.,0.},{40,130}};
+    /** French road pavement "Matching directive 2015/996/EC (CNOSSOS-EU)
+     and the French emission model for road pavements", Dutilleux, Soldano, Euronoise 2018**/
+     private static final double[][] RoadCoeff_FR1={ // R1 drainant (ajustement composante totale, Rapport Cerema 2016)
+            {13.9,23.3,19,0.,0.,0.},
+            {14,19.4,15.5,0.,0.,0.},
+            {14.1,15.5,12,0.,0.,0.},
+            {8.7,10.7,5.8,0.,0.,0.},
+            {-2.5,5.8,1.5,0.,0.,0.},
+            {-3.9,7.6,3.3,0.,0.,0.},
+            {-0.5,8.2,4.,0.,0.,0.},
+            {2.9,8.9,4.8,0.,0.,0.},
+            {-2.9,-0.1,6.6,0.,0.,0.},{30,130}};
+
+    private static final double[][] RoadCoeff_FR2={ // R1 non drainant(ajustement composante totale, Rapport Cerema 2016)
+            {9,18.6,14.2,0.,0.,0.},
+            {9.6,15.1,11.1,0.,0.,0.},
+            {10.2,11.7,8.1,0.,0.,0.},
+            {5.8,7.8,2.8,0.,0.,0.},
+            {-0.3,8.2,3.8,0.,0.,0.},
+            {-2.7,8.8,4.4,0.,0.,0.},
+            {-0.7,8.1,3.8,0.,0.,0.},
+            {1.3,7.5,3.3,0.,0.,0.},
+            {-3.4,-1.3,6.1,0.,0.,0.},{30,130}};
+
+    private static final double[][] RoadCoeff_FR3={ // R2 drainant(ajustement composante totale, Rapport Cerema 2016)
+            {15.7,25.4,21.4,0.,0.,0.},
+            {15.8,21.5,17.9,0.,0.,0.},
+            {15.9,17.6,14.4,15.4,0.,0.},
+            {10.5,12.8,8.2,0.,0.,0.},
+            {-0.7,7.9,3.9,0.,0.,0.},
+            {-2.1,9.7,5.7,0.,0.,0.},
+            {1.3,10.3,6.4,0.,0.,0.},
+            {4.7,11.,7.2,0.,0.,0.},
+            {-2.2,0.3,4.9,0.,0.,0.},{30,130}};
+
+    private static final double[][] RoadCoeff_FR4={ // R2 non drainant(ajustement composante totale, Rapport Cerema 2016)
+            {10.8,20.6,16.7,0.,0.,0.},
+            {11.4,17.1,13.6,0.,0.,0.},
+            {12,13.7,10.6,0.,0.,0.},
+            {7.5,9.8,5.3,0.,0.,0.},
+            {1.5,10.2,6.3,0.,0.,0.},
+            {-0.9,10.8,6.9,0.,0.,0.},
+            {1.1,10.1,6.3,0.,0.,0.},
+            {3.1,9.5,5.8,0.,0.,0.},
+            {-2.7,-1.2,3.5,0.,0.,0.},{30,130}};
+
+    private static final double[][] RoadCoeff_FR5={ // R3 drainant(ajustement composante totale, Rapport Cerema 2016)
+            {17.5,26.2,22.4,0.,0.,0.},
+            {17.5,22.3,18.9,0.,0.,0.},
+            {17.6,18.4,15.4,0.,0.,0.},
+            {12.3,13.6,9.2,0.,0.,0.},
+            {1,8.7,-1.8,4.9,0.,0.},
+            {-0.3,10.5,6.7,0.,0.,0.},
+            {3.1,11.1,7.4,0.,0.,0.},
+            {6.4,11.8,8.2,0.,0.,0.},
+            {0.1,0.4,4.3,0.,0.,0.},{30,130}};
+
+    private static final double[][] RoadCoeff_FR6={ // R3 non drainant(ajustement composante totale, Rapport Cerema 2016)
+            {12.7,21.5,17.7,0.,0.,0.},
+            {13.2,18,14.6,0.,0.,0.},
+            {13.8,14.6,11.6,0.,0.,0.},
+            {9.4,10.7,6.3,0.,0.,0.},
+            {3.4,11.1,7.3,0.,0.,0.},
+            {0.9,11.7,7.9,0.,0.,0.},
+            {3,11,7.3,0.,0.,0.},
+            {5,10.4,6.8,0.,0.,0.},
+            {-0.8,-0.8,2.8,0.,0.,0.},{30,130}};
 
     /** CNOSSOS Vehicule Tables -  All coefficients are from CNOSSOS-EU_Road_Catalogue_Final - 01April2014.xlsx - Emission + Studded **/
     private static final double[][] VehCat1={   // light vehicle
@@ -368,6 +435,24 @@ public class EvaluateRoadSourceCnossos {
                 break;
             case 14:
                 out_value=RoadCoeff_NL14[Freq_ind][VehCat_ind];
+                break;
+            case 15:
+                out_value=RoadCoeff_FR1[Freq_ind][VehCat_ind];
+                break;
+            case 16:
+                out_value=RoadCoeff_FR2[Freq_ind][VehCat_ind];
+                break;
+            case 17:
+                out_value=RoadCoeff_FR3[Freq_ind][VehCat_ind];
+                break;
+            case 18:
+                out_value=RoadCoeff_FR4[Freq_ind][VehCat_ind];
+                break;
+            case 19:
+                out_value=RoadCoeff_FR5[Freq_ind][VehCat_ind];
+                break;
+            case 20:
+                out_value=RoadCoeff_FR6[Freq_ind][VehCat_ind];
                 break;
             default :
                 out_value=0;
