8eda7fffe9c2ec3b3d3e5aa0141cd7f3e4f15e71
pierromond <pierromond@hotmail.com>
2018-10-19 16:57:53 +0200

    ...

--- noisemap-core/src/main/java/org/orbisgis/noisemap/core/jdbc/PointNoiseMap_f.java
+++ noisemap-core/src/main/java/org/orbisgis/noisemap/core/jdbc/PointNoiseMap_f.java
@@ -124,7 +124,7 @@ public class PointNoiseMap_f extends JdbcNoiseMap {
                 receivers, freeFieldFinder, sourcesIndex,
                 sourceGeometries, wj_sources, db_field_freq,
                 soundReflectionOrder, soundDiffractionOrder, maximumPropagationDistance, maximumReflectionDistance,
-                0, wallAbsorption, favrose, ij,
+                0, wallAbsorption, DEFAULT_WIND_ROSE, ij,
                 progression.subProcess(receivers.size()), geoWithSoil, computeVerticalDiffraction);
     }
 
