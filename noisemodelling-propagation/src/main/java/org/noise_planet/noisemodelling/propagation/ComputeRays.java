a1af5f9a4f9bd222772a6a99e92f925f65e401f3
pierromond <pierromond@hotmail.com>
2019-05-27 11:03:17 +0200

    fix small bug Z geometry

--- noisemodelling-propagation/src/main/java/org/noise_planet/noisemodelling/propagation/ComputeRays.java
+++ noisemodelling-propagation/src/main/java/org/noise_planet/noisemodelling/propagation/ComputeRays.java
@@ -165,7 +165,7 @@ public class ComputeRays {
      */
     public void makeSourceRelativeZToAbsolute() {
         AbsoluteCoordinateSequenceFilter filter = new AbsoluteCoordinateSequenceFilter(data.freeFieldFinder, true);
-        List<Geometry> sourceCopy = new ArrayList<>(data.sourceGeometries);
+        List<Geometry> sourceCopy = new ArrayList<>();
         for (Geometry source : data.sourceGeometries) {
             filter.reset();
             Geometry cpy = source.copy();
