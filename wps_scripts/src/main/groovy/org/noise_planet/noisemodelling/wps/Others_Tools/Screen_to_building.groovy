9e5d019dc980687ebf28c1f2be30d26e770530ab
pierromond <pierromond@hotmail.com>
2019-11-15 11:49:33 +0100

    ...

--- wps_scripts/src/main/groovy/org/noise_planet/noisemodelling/wps/Others_Tools/Screen_to_building.groovy
+++ wps_scripts/src/main/groovy/org/noise_planet/noisemodelling/wps/Others_Tools/Screen_to_building.groovy
@@ -4,40 +4,27 @@
 
 package org.noise_planet.noisemodelling.wps.Others_Tools
 
-import org.h2gis.functions.io.gpx.*
-import org.h2gis.functions.io.osm.*
-
 import org.h2gis.utilities.wrapper.*
-
 
 import geoserver.GeoServer
 import geoserver.catalog.Store
 import org.geotools.jdbc.JDBCDataStore
 import org.geotools.data.simple.*
 
-import java.sql.Connection;
+import java.sql.Connection
 import org.locationtech.jts.geom.Geometry
-import java.io.*
 import java.sql.*
 import groovy.sql.Sql
 
-import org.h2gis.functions.io.csv.*
-import org.h2gis.functions.io.dbf.*
-import org.h2gis.functions.io.geojson.*
-import org.h2gis.functions.io.json.*
-import org.h2gis.functions.io.kml.*
-import org.h2gis.functions.io.shp.*
-import org.h2gis.functions.io.tsv.*
 
-title = 'Receivers Grid around Buildings'
-description = 'Calculates a regular grid of receivers around buildings. Step is the step value of the grid in the Cartesian plane in meters.'
+title = 'Screen to Buildings'
+description = 'Screen to Buildings.'
 
 inputs = [buildingTableName : [name: 'Buildings table name', title: 'Buildings table name', min: 0, max: 1, type: String.class],
           fence  : [name: 'Fence', title: 'Fence', min: 0, max: 1, type: Geometry.class],
           screenTableName  : [name: 'Screen table name', title: 'Sources table name', type: String.class],
           databaseName   : [name: 'Name of the database', title: 'Name of the database', description: 'Name of the database. (default : h2gisdb)', min: 0, max: 1, type: String.class],
-          outputTableName: [name: 'outputTableName', description: 'Do not write the name of a table that contains a space. (default : SCREENS)', title: 'Name of output table', min: 0, max: 1, type: String.class],
-          height    : [name: 'height', title: 'height', description: 'Height of receivers in meters', min: 0, max: 1, type: Double.class]]
+          outputTableName: [name: 'outputTableName', description: 'Do not write the name of a table that contains a space. (default : SCREENS)', title: 'Name of output table', min: 0, max: 1, type: String.class]]
 
 
 
@@ -51,7 +38,7 @@ def static Connection openPostgreSQLDataStoreConnection(String dbName) {
 
 def run(input) {
 
-    String screen_table_name = "Ecran_zone_étude_FINAL"
+    String screen_table_name = "Ecran_zone_etude_FINAL"
     if (input['screenTableName']) {
         screen_table_name = input['screenTableName']
     }
@@ -99,10 +86,17 @@ def run(input) {
         //sql.execute("Create spatial index on SCREENS_2154(the_geom);")
 
         if (input['buildingTableName']) {
-            // add Fusion table
+
+            sql.execute("DROP TABLE IF EXISTS BUFFERED_SCREENS")
+            sql.execute("CREATE TABLE BUFFERED_SCREENS as select ST_BUFFER(sc.the_geom,0.2, 'endcap=flat') the_geom,  Hmax HEIGHT, Absorbtion A from SCREENS sc, FENCE_2154 fe WHERE sc.the_geom && fe.the_geom AND ST_INTERSECTS(sc.the_geom, fe.the_geom) ")
+
             sql.execute("DROP TABLE IF EXISTS BUILDINGS_SCREENS")
-            sql.execute("CREATE TABLE BUILDINGS_SCREENS as select ST_BUFFER(sc.the_geom,0.2, 'endcap=flat') the_geom,  Hmax HEIGHT, Absorbtion A from SCREENS sc, FENCE_2154 fe WHERE sc.the_geom && fe.the_geom AND ST_INTERSECTS(sc.the_geom, fe.the_geom) ")
-                }else{
+            sql.execute("CREATE TABLE BUILDINGS_SCREENS as select the_geom, HEIGHT, A from BUFFERED_SCREENS sc UNION select the_geom, HEIGHT, null from BUILDINGS ")
+
+            sql.execute("DROP TABLE IF EXISTS BUFFERED_SCREENS")
+
+
+        }else{
             sql.execute("DROP TABLE IF EXISTS BUILDINGS_SCREENS")
             sql.execute("CREATE TABLE BUILDINGS_SCREENS as select ST_BUFFER(sc.the_geom,0.2, 'endcap=flat')  the_geom,  Hmax HEIGHT, Absorbtion A from SCREENS sc, FENCE_2154 fe WHERE sc.the_geom && fe.the_geom AND ST_INTERSECTS(sc.the_geom, fe.the_geom)")
         }
