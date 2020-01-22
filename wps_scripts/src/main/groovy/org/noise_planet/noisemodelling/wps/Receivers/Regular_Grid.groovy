a84a80bcb0a5dac03994efa0f4db70ae8bd8413b
pierromond <pierromond@hotmail.com>
2020-01-22 13:32:55 +0100

    - Merge

--- wps_scripts/src/main/groovy/org/noise_planet/noisemodelling/wps/Receivers/Regular_Grid.groovy
+++ wps_scripts/src/main/groovy/org/noise_planet/noisemodelling/wps/Receivers/Regular_Grid.groovy
@@ -19,18 +19,20 @@ inputs = [buildingTableName : [name: 'Buildings table name', title: 'Buildings t
           fenceTableName  : [name: 'Fence table name', title: 'Fence table name', min: 0, max: 1, type: String.class],
           sourcesTableName  : [name: 'Sources table name', title: 'Sources table name', min: 0, max: 1, type: String.class],
           delta    : [name: 'offset', title: 'offset', description: 'Offset in the Cartesian plane in meters', type: Double.class],
-          databaseName   : [name: 'Name of the database', title: 'Name of the database', description: 'Name of the database. (default : h2gisdb)', min: 0, max: 1, type: String.class],
+          databaseName   : [name: 'Name of the database', title: 'Name of the database', description: 'Name of the database (default : first found db)', min: 0, max: 1, type: String.class],
           receiverstablename: [name: 'receiverstablename', description: 'Do not write the name of a table that contains a space. (default : RECEIVERS)', title: 'Name of receivers table', min: 0, max: 1, type: String.class],
           height    : [name: 'height', title: 'height', description: 'Height of receivers in meters', min: 0, max: 1, type: Double.class]]
 
 outputs = [tableNameCreated: [name: 'tableNameCreated', title: 'tableNameCreated', type: String.class]]
 
-def static Connection openPostgreSQLDataStoreConnection(String dbName) {
+static Connection openGeoserverDataStoreConnection(String dbName) {
+    if(dbName == null || dbName.isEmpty()) {
+        dbName = new GeoServer().catalog.getStoreNames().get(0)
+    }
     Store store = new GeoServer().catalog.getStore(dbName)
-    JDBCDataStore jdbcDataStore = (JDBCDataStore) store.getDataStoreInfo().getDataStore(null)
+    JDBCDataStore jdbcDataStore = (JDBCDataStore)store.getDataStoreInfo().getDataStore(null)
     return jdbcDataStore.getDataSource().getConnection()
 }
-
 def run(input) {
 
     String receivers_table_name = "RECEIVERS"
@@ -68,31 +70,28 @@ def run(input) {
     }
     building_table_name = building_table_name.toUpperCase()
 
-    System.out.println("--------------------------------------------")
     String fence = null
     if (input['fence']) {
         fence = (String) input['fence']
     }
 
     // Get name of the database
-    String dbName = "h2gisdb"
+    String dbName = ""
     if (input['databaseName']) {
         dbName = input['databaseName'] as String
     }
 
     // Open connection
-    openPostgreSQLDataStoreConnection(dbName).withCloseable { Connection connection ->
-        //Statement sql = connection.createStatement()
+    openGeoserverDataStoreConnection(dbName).withCloseable { Connection connection ->
+
         Sql sql = new Sql(connection)
-        System.out.println("Delete previous receivers grid...")
+       //Delete previous receivers grid.
         sql.execute(String.format("DROP TABLE IF EXISTS %s", receivers_table_name))
         String queryGrid = null
 
 
 
         if (input['fence']) {
-            System.out.println("--------------------------------------------")
-            System.out.println((String) fence)
             sql.execute(String.format("DROP TABLE IF EXISTS FENCE"))
             sql.execute(String.format("CREATE TABLE FENCE AS SELECT ST_AsText('"+ fence + "') the_geom"))
             sql.execute(String.format("DROP TABLE IF EXISTS FENCE_2154"))
@@ -122,7 +121,7 @@ def run(input) {
 
         sql.execute(queryGrid)
 
-         System.out.println("New receivers grid created ...")
+         //New receivers grid created .
 
         sql.execute("Create spatial index on "+receivers_table_name+"(the_geom);")
         sql.execute("UPDATE "+receivers_table_name+" SET THE_GEOM = ST_UPDATEZ(The_geom,"+h+");")
@@ -132,30 +131,30 @@ def run(input) {
         sql.execute("ALTER TABLE "+ receivers_table_name +" DROP ID_ROW;" )
 
         if (input['fence']) {
-            System.out.println("Delete receivers  ...")
+            //Delete receivers
             sql.execute("Create spatial index on FENCE_2154(the_geom);")
             sql.execute("delete from " + receivers_table_name + " g where exists (select 1 from FENCE_2154 r where ST_Disjoint(g.the_geom, r.the_geom) limit 1);")
         }
         if (input['fenceTableName']) {
-            System.out.println("Delete receivers ...")
+            //Delete receivers
             sql.execute("Create spatial index on "+fence_table_name+"(the_geom);")
             sql.execute("delete from " + receivers_table_name + " g where exists (select 1 from "+fence_table_name+" r where ST_Disjoint(g.the_geom, r.the_geom) limit 1);")
         }
 
         if (input['buildingTableName']) {
-            System.out.println("Delete receivers inside buildings ...")
+            //Delete receivers inside buildings
             sql.execute("Create spatial index on "+building_table_name+"(the_geom);")
             sql.execute("delete from "+receivers_table_name+" g where exists (select 1 from "+building_table_name+" b where ST_Z(g.the_geom) < b.HEIGHT and g.the_geom && b.the_geom and ST_INTERSECTS(g.the_geom, b.the_geom) and ST_distance(b.the_geom, g.the_geom) < 1 limit 1);")
         }
         if (input['sourcesTableName']) {
-            System.out.println("Delete receivers near sources ...")
+            //Delete receivers near sources
             sql.execute("Create spatial index on "+sources_table_name+"(the_geom);")
             sql.execute("delete from "+receivers_table_name+" g where exists (select 1 from "+sources_table_name+" r where st_expand(g.the_geom, 1) && r.the_geom and st_distance(g.the_geom, r.the_geom) < 1 limit 1);")
         }
 
 
     }
-    System.out.println("Process Done !")
-    return [tableNameCreated: "Process done. Table of receivers "+ receivers_table_name +" created !"]
+
+     return [tableNameCreated: "Process done. Table of receivers "+ receivers_table_name +" created !"]
 }
 
