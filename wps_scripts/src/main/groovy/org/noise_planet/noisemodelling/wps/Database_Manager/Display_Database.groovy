8b3f24bff9452dd31625adb0fbf14f31ebd0fa20
pierromond <pierromond@hotmail.com>
2020-03-03 09:57:06 +0100

    Change Header

--- wps_scripts/src/main/groovy/org/noise_planet/noisemodelling/wps/Database_Manager/Display_Database.groovy
+++ wps_scripts/src/main/groovy/org/noise_planet/noisemodelling/wps/Database_Manager/Display_Database.groovy
@@ -1,32 +1,20 @@
 /**
  * NoiseModelling is a free and open-source tool designed to produce environmental noise maps on very large urban areas. It can be used as a Java library or be controlled through a user friendly web interface.
  *
- * This version is developed at French IRSTV Institute and at IFSTTAR
- * (http://www.ifsttar.fr/) as part of the Eval-PDU project, funded by the
- * French Agence Nationale de la Recherche (ANR) under contract ANR-08-VILL-0005-01.
+ * This version is developed by Université Gustave Eiffel and CNRS
+ * <http://noise-planet.org/noisemodelling.html>
+ * as part of:
+ * the Eval-PDU project (ANR-08-VILL-0005) 2008-2011, funded by the Agence Nationale de la Recherche (French)
+ * the CENSE project (ANR-16-CE22-0012) 2017-2021, funded by the Agence Nationale de la Recherche (French)
+ * the Nature4cities (N4C) project, funded by European Union’s Horizon 2020 research and innovation programme under grant agreement No 730468
  *
- * Noisemap is distributed under GPL 3 license. Its reference contact is Judicaël
- * Picaut <judicael.picaut@ifsttar.fr>. It is maintained by Nicolas Fortin
- * as part of the "Atelier SIG" team of the IRSTV Institute <http://www.irstv.fr/>.
+ * Noisemap is distributed under GPL 3 license.
  *
- * Copyright (C) 2011 IFSTTAR
- * Copyright (C) 2011-2012 IRSTV (FR CNRS 2488)
+ * Contact: contact@noise-planet.org
  *
- * Noisemap is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- * Noisemap is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * Noisemap. If not, see <http://www.gnu.org/licenses/>.
- *
- * For more information, please consult: <http://www.orbisgis.org/>
- * or contact directly:
- * info_at_ orbisgis.org
+ * Copyright (C) 2011-2012 IRSTV (FR CNRS 2488) and Ifsttar
+ * Copyright (C) 2013-2019 Ifsttar and CNRS
+ * Copyright (C) 2020 Université Gustave Eiffel and CNRS
  *
  * @Author Nicolas Fortin, Univ Gustave Eiffel
  */
