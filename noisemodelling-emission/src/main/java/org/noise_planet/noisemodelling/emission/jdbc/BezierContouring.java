48a9e75f2027e2db77ff6ecaf15ab7d5ebc97788
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2020-05-13 15:02:57 +0200

    before opti

--- noisemodelling-emission/src/main/java/org/noise_planet/noisemodelling/emission/jdbc/BezierContouring.java
+++ noisemodelling-emission/src/main/java/org/noise_planet/noisemodelling/emission/jdbc/BezierContouring.java
@@ -17,7 +17,7 @@ import java.sql.*;
 import java.util.*;
 
 public class BezierContouring {
-    static final int NUM_STEPS = 20;
+    static final int NUM_STEPS = 10;
     static final int BATCH_MAX_SIZE = 500;
     String pointTable = "LDEN_RESULT";
     String triangleTable = "TRIANGLES";
@@ -73,10 +73,9 @@ public class BezierContouring {
         double xc3 = (x2 + x3) / 2.0;
         double yc3 = (y2 + y3) / 2.0;
 
-        double len1 = Math.sqrt((x1-x0) * (x1-x0) + (y1-y0) * (y1-y0));
-        double lentest = p0.distance(p1);
-        double len2 = Math.sqrt((x2-x1) * (x2-x1) + (y2-y1) * (y2-y1));
-        double len3 = Math.sqrt((x3-x2) * (x3-x2) + (y3-y2) * (y3-y2));
+        double len1 = p0.distance(p1);
+        double len2 = p1.distance(p2);
+        double len3 = p2.distance(p3);
 
         double k1 = len1 / (len1 + len2);
         double k2 = len2 / (len2 + len3);
@@ -160,14 +159,63 @@ public class BezierContouring {
 
     static Coordinate[] interpolate(Coordinate[] coordinates, double smoothValue) {
         ArrayList<Coordinate> pts = new ArrayList<>(coordinates.length * NUM_STEPS);
+        Coordinate[] midPoint = new Coordinate[coordinates.length];
+        double[] len = new double[coordinates.length];
+        for(int i = 0; i < coordinates.length; i++) {
+            final int i2 = i + 1 >= coordinates.length ? i + 1 - coordinates.length : i + 1;
+            midPoint[i] = new Coordinate((coordinates[i].x + coordinates[i2].x) / 2, (coordinates[i].y + coordinates[i2].y) / 2);
+            len[i] = coordinates[i].distance(coordinates[i2]);
+        }
+
         pts.add(coordinates[0]);
         for(int i = 0; i < coordinates.length; i++) {
-            Coordinate p0 = i - 1 < 0 ? coordinates[coordinates.length - 1] : coordinates[i - 1];
+            final int i0 = i - 1 < 0 ? coordinates.length - 1 : i - 1;
+            final int i2 = i + 1 >= coordinates.length ? i + 1 - coordinates.length : i + 1;
+            final int i3 = i + 2 >= coordinates.length ? i + 2 - coordinates.length : i + 2;
+
+            Coordinate p0 = coordinates[i0];
             Coordinate p1 = coordinates[i];
-            Coordinate p2 = i + 1 >= coordinates.length ? coordinates[i + 1 - coordinates.length] : coordinates[i + 1];
-            Coordinate p3 = i + 2 >= coordinates.length ? coordinates[i + 2 - coordinates.length] : coordinates[i + 2];
-            Coordinate[] anchors = computeControlPoints(p0, p1, p2, p3, smoothValue);
-            pts.addAll(curve4(p1, anchors[0], anchors[1], p2));
+            Coordinate p2 = coordinates[i2];
+            Coordinate p3 = coordinates[i3];
+
+            final double x0 = p0.x;
+            final double y0 = p0.y;
+            final double x1 = p1.x;
+            final double y1 = p1.y;
+            final double x2 = p2.x;
+            final double y2 = p2.y;
+            final double x3 = p3.x;
+            final double y3 = p3.y;
+
+            double xc1 = (x0 + x1) / 2.0;
+            double yc1 = (y0 + y1) / 2.0;
+            double xc2 = (x1 + x2) / 2.0;
+            double yc2 = (y1 + y2) / 2.0;
+            double xc3 = (x2 + x3) / 2.0;
+            double yc3 = (y2 + y3) / 2.0;
+
+            double len1 = p0.distance(p1);
+            double len2 = p1.distance(p2);
+            double len3 = p2.distance(p3);
+
+            double k1 = len1 / (len1 + len2);
+            double k2 = len2 / (len2 + len3);
+
+            double xm1 = xc1 + (xc2 - xc1) * k1;
+            double ym1 = yc1 + (yc2 - yc1) * k1;
+
+            double xm2 = xc2 + (xc3 - xc2) * k2;
+            double ym2 = yc2 + (yc3 - yc2) * k2;
+
+            // Resulting control points. Here smooth_value is mentioned
+            // above coefficient K whose value should be in range [0...1].
+            double ctrl1_x = xm1 + (xc2 - xm1) * smoothValue + x1 - xm1;
+            double ctrl1_y = ym1 + (yc2 - ym1) * smoothValue + y1 - ym1;
+
+            double ctrl2_x = xm2 + (xc2 - xm2) * smoothValue + x2 - xm2;
+            double ctrl2_y = ym2 + (yc2 - ym2) * smoothValue + y2 - ym2;
+
+            pts.addAll(curve4(p1, new Coordinate(ctrl1_x, ctrl1_y), new Coordinate(ctrl2_x, ctrl2_y), p2));
         }
 
         return pts.toArray(new Coordinate[0]);
