aa0bbe972320951b5424e2c9fd2fe6d3c4d8e4e1
nicolas-f <github@nettrader.fr>
2022-01-25 09:45:32 +0100

    Apply change on  unit test

--- noisemodelling-jdbc/src/main/java/org/noise_planet/noisemodelling/jdbc/PointNoiseMap.java
+++ noisemodelling-jdbc/src/main/java/org/noise_planet/noisemodelling/jdbc/PointNoiseMap.java
@@ -323,6 +323,11 @@ public class PointNoiseMap extends JdbcNoiseMap {
             this.latitudeIndex = latitudeIndex;
         }
 
+        @Override
+        public String toString() {
+            return String.format("CellIndex(%d, %d);", longitudeIndex, latitudeIndex);
+        }
+
         public int getLongitudeIndex() {
             return longitudeIndex;
         }
