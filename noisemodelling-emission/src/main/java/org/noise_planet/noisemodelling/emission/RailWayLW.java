35b4110bed787fee20a89aa9c81cc1fe648ccfa1
Pierre Aumond <pierromond@hotmail.com>
2022-03-23 11:50:54 +0100

    fix small directivity errors

--- noisemodelling-emission/src/main/java/org/noise_planet/noisemodelling/emission/RailWayLW.java
+++ noisemodelling-emission/src/main/java/org/noise_planet/noisemodelling/emission/RailWayLW.java
@@ -145,15 +145,15 @@ public class RailWayLW {
         int height_index = TRAIN_NOISE_SOURCE_H_INDEX[noiseSource.ordinal()];
         double attHorizontal = 10 * Math.log10(0.01 + 0.99 * Math.pow(Math.sin(phi), 2));
         double attVertical = 0;
-        if(height_index == 1) {
+        if(height_index == 0) {
             if(theta > 0 && theta <= Math.PI / 2.0) {
                 attVertical = (40.0 / 3.0)
                         * (2.0 / 3.0 * Math.sin(2 * theta) - Math.sin(theta))
                         * Math.log10((frequency + 600.0) / 200.0);
             }
-        } else if(height_index == 2){
+        } else if(height_index == 1){
             if(theta < 0) { // for aerodynamic effect only
-                attVertical = 10 * Math.log10(Math.pow(Math.sin(theta), 2));
+                attVertical = 10 * Math.log10(Math.pow(Math.cos(theta), 2));
             }
         }
         return attHorizontal + attVertical;
