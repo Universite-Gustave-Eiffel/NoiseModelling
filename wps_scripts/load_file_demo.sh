f00ae4832bf55f5e3cb5994de949f9c7b29afa3d
nicolas-f <github_account@nettrader.fr>
2022-05-25 09:27:57 +0200

    add quick start script

--- wps_scripts/load_file_demo.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-# create (or load existing) database and load a shape file into the database
-./bin/wps_scripts -w./ -snoisemodelling/wps/Import_and_Export/Import_File.groovy pathFile=resources/org/noise_planet/noisemodelling/wps/buildings.shp
-
