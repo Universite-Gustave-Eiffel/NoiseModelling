1ef891f0fb2c3f63af40384c6f8eccdfb6c3edcd
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2023-03-15 15:06:32 +0100

    fix plot

--- noisemodelling-jdbc/src/test/java/org/noise_planet/noisemodelling/jdbc/DirectivityTest.java
+++ noisemodelling-jdbc/src/test/java/org/noise_planet/noisemodelling/jdbc/DirectivityTest.java
@@ -5,6 +5,8 @@ import org.h2gis.utilities.JDBCUtilities;
 import org.junit.Test;
 import org.noise_planet.noisemodelling.emission.DiscreteDirectionAttributes;
 import org.noise_planet.noisemodelling.emission.RailWayLW;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
@@ -19,23 +21,27 @@ import java.util.Map;
 public class DirectivityTest {
     final static double[] freqTest = new double[] {125, 250, 500, 1000, 2000, 4000, 8000, 16000};
 
+    /**
+     * Create cardioid using equation from
+     * Farina, Angelo & Capra, Andrea & Chiesi, Lorenzo & Scopece, Leonardo. (2010). A Spherical Microphone Array for
+     * Synthesizing Virtual Directive Microphones in Live Broadcasting and in Post Production.
+     * @throws Exception
+     */
     @Test
     public void exportDirectivityCardioid() throws Exception {
         PolarGraphDirectivity polarGraphDirectivity = new PolarGraphDirectivity();
         Connection connection = JDBCUtilities.wrapConnection(H2GISDBFactory.createSpatialDataBase(DirectivityTableLoaderTest.class.getSimpleName(), true, ""));
         Statement st = connection.createStatement();
-        st.execute("CREATE TABLE DIRECTIVITY(DIR_ID INTEGER, THETA FLOAT, PHI FLOAT, LW500 FLOAT)");
-        PreparedStatement pst = connection.prepareStatement("INSERT INTO DIRECTIVITY VALUES(?, ?, ?, ?)");
-        for(int phi = 0; phi < 360; phi+=2) {
-            for(int theta=-90; theta <= 90; theta += 1) {
-                pst.setInt(1, 1);
-                pst.setDouble(2, theta);
-                pst.setDouble(3, phi);
-                double att =  30 * (Math.cos(Math.toRadians(phi)) * Math.cos(Math.toRadians(theta)));
-                pst.setDouble(4, att);
-                pst.execute();
-            }
-        }
+        double back_attenuation = -40;
+        double omnidirectionalFactor = 5; // 0=omnidirectional 1=cardioid 2="super cardioid" ..
+
+        PreparedStatement pst = connection.prepareStatement("CREATE TABLE DIRECTIVITY(DIR_ID INTEGER, THETA FLOAT, PHI FLOAT, LW500 FLOAT)" +
+                " AS SELECT 1, THETA, PHI, ? * (1 - POWER(0.5 + 0.5 * COS(RADIANS(PHI)) * COS(RADIANS(THETA)), ?)) FROM" +
+                "  (SELECT X PHI FROM SYSTEM_RANGE(0, 355, 5)) T1, (SELECT X THETA FROM SYSTEM_RANGE(-90, 90, 5)) T2");
+        pst.setDouble(1, back_attenuation);
+        pst.setDouble(2, omnidirectionalFactor);
+        pst.execute();
+        // DEBUG st.execute("UPDATE DIRECTIVITY SET LW500=-10 WHERE THETA=45 AND PHI=270 ");
         // Data is inserted now fetch it from the database
         Map<Integer, DiscreteDirectionAttributes> directivities =
                 DirectivityTableLoader.loadTable(connection, "DIRECTIVITY", 1);
@@ -46,7 +52,7 @@ public class DirectivityTest {
                     "<style>" +
                     ".wrapper {\n" +
                     "  display: grid;\n" +
-                    "  grid-template-columns: repeat(3, 1fr);\n" +
+                    "  grid-template-columns: repeat(2, 1fr);\n" +
                     "  grid-gap: 10px;\n" +
                     "  grid-auto-rows: minmax(100px, auto);\n" +
                     "}" +
@@ -65,7 +71,10 @@ public class DirectivityTest {
                     "}" +
                     "</style>" +
                     "<head><body><h1>500 Hz</h1>\n");
+            Logger logger = LoggerFactory.getLogger(DirectivityTest.class);
             for (Map.Entry<Integer, DiscreteDirectionAttributes> entry : directivities.entrySet()) {
+                // DEBUG logger.info(String.format("phi 0 theta 0 %f",
+                //       entry.getValue().getAttenuation(500, Math.toRadians(0), Math.toRadians(0))));
                 bw.write("<div class=\"wrapper\">");
                 bw.write("<div class=\"one\">");
                 bw.write(polarGraphDirectivity.generatePolarGraph(entry.getValue(), 500,
