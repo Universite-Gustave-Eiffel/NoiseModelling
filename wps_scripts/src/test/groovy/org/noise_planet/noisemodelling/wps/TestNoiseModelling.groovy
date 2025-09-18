67ef6781a25fb6cd02d751ece27df3906c5170de
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2025-09-18 14:03:13 +0200

    Add new unit test for testing noise level from traffic with the tableRoadsTraffic specified (with period field)

--- wps_scripts/src/test/groovy/org/noise_planet/noisemodelling/wps/TestNoiseModelling.groovy
+++ wps_scripts/src/test/groovy/org/noise_planet/noisemodelling/wps/TestNoiseModelling.groovy
@@ -408,4 +408,69 @@ class TestNoiseModelling extends JdbcTestCase {
 
         LOGGER.info(Arrays.toString(fieldNames.toArray()))
     }
+
+
+    void testNoiseFromTrafficUsingPeriod() {
+        new Import_File().exec(connection,
+                ["pathFile" : TestNoiseModelling.getResource("ROADS2.shp").getPath()])
+
+        // Create SOURCES_EMISSION table by splitting the ROADS2 table old format fields to two tables
+
+        Sql sql = new Sql(connection)
+        sql.execute("DROP TABLE IF EXISTS SOURCES_EMISSION")
+        sql.execute("CREATE TABLE SOURCES_TRAFFIC AS SELECT PK AS IDSOURCE, 'D' AS PERIOD," +
+                " TV_D as TV, HV_D as HV, LV_SPD_D as LV_SPD, HV_SPD_D as HV_SPD," +
+                " PVMT AS PVMT FROM ROADS2")
+        sql.execute("INSERT INTO SOURCES_TRAFFIC SELECT PK AS IDSOURCE, 'E' AS PERIOD," +
+                " TV_E as TV, HV_E as HV, LV_SPD_E as LV_SPD, HV_SPD_E as HV_SPD," +
+                " PVMT AS PVMT FROM ROADS2")
+        sql.execute("INSERT INTO SOURCES_TRAFFIC SELECT PK AS IDSOURCE, 'N' AS PERIOD," +
+                " TV_N as TV, HV_N as HV, LV_SPD_N as LV_SPD, HV_SPD_N as HV_SPD," +
+                " PVMT AS PVMT FROM ROADS2")
+        // create a table SOURCES_GEOM with only the geometry of ROADS2
+        sql.execute("DROP TABLE IF EXISTS SOURCES_GEOM")
+        sql.execute("CREATE TABLE SOURCES_GEOM(pk integer primary key, the_geom geometry) AS SELECT PK , THE_GEOM FROM ROADS2")
+
+
+        // Import buildings and receivers
+        new Import_File().exec(connection,
+                ["pathFile" : TestNoiseModelling.getResource("buildings.shp").getPath(),
+                 "inputSRID": "2154",
+                 "tableName": "buildings"])
+
+        new Import_File().exec(connection,
+                ["pathFile" : TestNoiseModelling.getResource("receivers.shp").getPath(),
+                 "inputSRID": "2154",
+                 "tableName": "receivers"])
+
+        // Run propagation
+
+        String res = new Noise_level_from_traffic().exec(connection,
+                ["tableBuilding"   : "BUILDINGS",
+                 "tableRoads"   : "SOURCES_GEOM",
+                 "tableRoadsTraffic": "SOURCES_TRAFFIC",
+                 "tableReceivers": "RECEIVERS"])
+
+        assertTrue(res.contains(NoiseMapDatabaseParameters.DEFAULT_RECEIVERS_LEVEL_TABLE_NAME))
+
+        def leqs = sql.firstRow("SELECT MAX(HZ63) , MAX(HZ125), MAX(HZ250), MAX(HZ500), MAX(HZ1000)," +
+                " MAX(HZ2000), MAX(HZ4000), MAX(HZ8000) FROM " +
+                NoiseMapDatabaseParameters.DEFAULT_RECEIVERS_LEVEL_TABLE_NAME + " WHERE PERIOD = 'D'")
+
+        assertEquals(87, leqs[0] as Double, 2.0)
+        assertEquals(78, leqs[1] as Double, 2.0)
+        assertEquals(78, leqs[2] as Double, 2.0)
+        assertEquals(79, leqs[3] as Double, 2.0)
+        assertEquals(82, leqs[4] as Double, 2.0)
+        assertEquals(80, leqs[5] as Double, 2.0)
+        assertEquals(71, leqs[6] as Double, 2.0)
+        assertEquals(62, leqs[7] as Double, 2.0)
+
+        // Output fields export period in a separate field now
+        def gotPeriod = JDBCUtilities.getUniqueFieldValues(connection, NoiseMapDatabaseParameters.DEFAULT_RECEIVERS_LEVEL_TABLE_NAME, "PERIOD")
+        assertEquals(3, gotPeriod.size())
+        assertTrue(gotPeriod.contains("D"))
+        assertTrue(gotPeriod.contains("E"))
+        assertTrue(gotPeriod.contains("N"))
+    }
 }
