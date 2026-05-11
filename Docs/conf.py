985539a32df69d99b9ab520873621f3ba1f1fac2
Martin Glesser <martin.glesser@univ-eiffel.fr>
2026-05-11 15:44:23 +0200

    docs: remove sphinx html_static_path since the folder does not exists

--- Docs/conf.py
+++ Docs/conf.py
@@ -119,7 +119,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 # Get rid of duplicate label warnings when using several time the same
 # title
